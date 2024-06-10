eb30b0794556d04bc1794e325257bc56af2e41af
Jason Carnahan <jasoncarnahan@canvas-inc.com>
2024-06-10 18:49:44 -0500

    Vertex Shader and Style Config

--- main.cpp
+++ main.cpp
@@ -1,39 +1,75 @@
-#include <iostream>
-#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glad/glad.h>
+#include <iostream>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+constexpr unsigned int SCREEN_WIDTH = 1280;
+constexpr unsigned int SCREEN_HEIGHT = 720;
+
+const char *vertexShaderSource = "#version 330 core\n"
+                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "void main()\n"
+                                 "{\n"
+                                 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                 "}\0";
+
+void framebufferSizeCallback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
 
 int main() {
-    std::cout << "Hello, OpenGL!" << std::endl;
+    std::cout << "Hello, OpenGL!\n";
 
+    // GLFW Init and Config
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "Modern OpenGL", nullptr, nullptr);
-
-    if (window == nullptr)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
+    // Create the window
+    GLFWwindow *window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Modern OpenGL", nullptr, nullptr);
+    if (window == nullptr) {
+        std::cout << "Failed to create GLFW window\n";
         glfwTerminate();
         return -1;
     }
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
 
-    if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress)))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
+    // Load Glad - OpenGL Function pointers
+    if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress))) {
+        std::cout << "Failed to initialize GLAD\n";
         return -1;
     }
 
-    glViewport(0, 0, 1280, 720);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    // Vertex Input - 3 points of a triangle
+    float vertices[] = {
+        -0.5F, -0.5F, 0.0F, //
+        0.5F,  -0.5F, 0.0F, //
+        0.0F,  0.5F,  0.0F  //
+    };
 
-    while(!glfwWindowShouldClose(window))
-    {
+    // Store the verticies on the memory for the GPU for the graphics pipeline
+    unsigned int vertexBufferObject;
+    glGenBuffers(1, &vertexBufferObject);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Create Vertex Shader then attach the shader source code to the shader
+    // object and compile the shader
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertexShader);
+
+    // Check shader compilation for errors
+    int success;
+    char info_log[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, nullptr, info_log);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << info_log << "\n";
+    }
+
+    // Rendera Loop
+    while (!glfwWindowShouldClose(window)) {
         // Input handle
         processInput(window);
 
@@ -41,7 +77,7 @@ int main() {
         glClearColor(1.0F, 0.25F, 1.0F, 1.0F);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // check and call events and swap the buffers
+        // Swap buffers and check for events
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -50,12 +86,19 @@ int main() {
     return 0;
 }
 
-void framebuffer_size_callback(GLFWwindow* window, const int width, const int height)
-{
+/**
+ * Query GLFW whether relevant keys are pressed/released this frame and react
+ * accordingly.
+ */
+void framebufferSizeCallback(GLFWwindow *window, const int width, const int height) {
     glViewport(0, 0, width, height);
 }
 
-void processInput(GLFWwindow *window)
-{
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) glfwSetWindowShouldClose(window, true);
+/**
+ * Executes whenever the window size changed.
+ */
+void processInput(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
 }
